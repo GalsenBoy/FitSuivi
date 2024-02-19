359e67db21577e320246ed3e3592dbbdd747c710
bilaly <billycsk@gmail.com>
2024-02-19 16:54:56 +0100

    Ajout des dépendancesUi + squellette de la navigation

---
+++ components/navigation/Navigation.tsx
@@ -0,0 +1,16 @@
+import "./navigation.scss";
+export default function Navigation() {
+  return (
+    <nav>
+      <div className="">
+        <a href="">Statistiques</a>
+        <a href="">Objectif</a>
+        <a href="">Bien être</a>
+      </div>
+      <div>
+        <a href="">Paramètres</a>
+        <a href="">Se connecter</a>
+      </div>
+    </nav>
+  );
+}
