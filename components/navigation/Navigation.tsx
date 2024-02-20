3a47b68383c71f36faa3690f4bfbc2a8714e2f99
bilaly <billycsk@gmail.com>
2024-02-20 11:54:24 +0100

    Ajout d'icône et optimisation du css

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,15 +1,52 @@
 import "./navigation.scss";
+import { TbTargetArrow } from "react-icons/tb";
+import { IoMdSettings } from "react-icons/io";
+import { FaChartLine, FaHeartPulse } from "react-icons/fa6";
+import { GiFruitBowl } from "react-icons/gi";
+import { SlLogin } from "react-icons/sl";
+
 export default function Navigation() {
   return (
     <nav>
-      <div className="">
-        <a href="">Statistiques</a>
-        <a href="">Objectif</a>
-        <a href="">Bien être</a>
+      <div className="flex">
+        <a href="">
+          <span>
+            <FaChartLine />
+          </span>{" "}
+          Statistiques
+        </a>
+        <a href="">
+          <span>
+            <TbTargetArrow />
+          </span>
+          Objectif
+        </a>
+        <a href="">
+          <span>
+            <GiFruitBowl />
+          </span>
+          Regime
+        </a>
+        <a href="">
+          <span>
+            <FaHeartPulse />
+          </span>
+          Bien être
+        </a>
       </div>
       <div>
-        <a href="">Paramètres</a>
-        <a href="">Se connecter</a>
+        <a href="">
+          <span>
+            <IoMdSettings />
+          </span>
+          Paramètres
+        </a>
+        <a href="">
+          <span>
+            <SlLogin />
+          </span>
+          Se connecter
+        </a>
       </div>
     </nav>
   );
