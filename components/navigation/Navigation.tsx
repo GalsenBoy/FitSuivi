d1da6489a149bba5071fdb0bac028f33d8a1cd25
bilaly <billycsk@gmail.com>
2024-02-21 16:16:46 +0100

    Finition de sidebar + Ajout header

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -7,47 +7,50 @@ import { SlLogin } from "react-icons/sl";
 
 export default function Navigation() {
   return (
-    <nav>
-      <div className="flex">
-        <a href="">
-          <span>
-            <FaChartLine />
-          </span>{" "}
-          Statistiques
-        </a>
-        <a href="">
-          <span>
-            <TbTargetArrow />
-          </span>
-          Objectif
-        </a>
-        <a href="">
-          <span>
-            <GiFruitBowl />
-          </span>
-          Regime
-        </a>
-        <a href="">
-          <span>
-            <FaHeartPulse />
-          </span>
-          Bien être
-        </a>
-      </div>
-      <div>
-        <a href="">
-          <span>
-            <IoMdSettings />
-          </span>
-          Paramètres
-        </a>
-        <a href="">
-          <span>
-            <SlLogin />
-          </span>
-          Se connecter
-        </a>
-      </div>
-    </nav>
+    <aside style={{ gridRow: "1/2" }}>
+      <nav id="navigation">
+        <div className="flex mt-5" id="nav-items">
+          <h1 className="text-2xl mb-6">FitSuivi</h1>
+          <a className="" href="">
+            <span>
+              <FaChartLine />
+            </span>{" "}
+            Statistiques
+          </a>
+          <a href="">
+            <span>
+              <TbTargetArrow />
+            </span>
+            Objectif
+          </a>
+          <a href="">
+            <span>
+              <GiFruitBowl />
+            </span>
+            Regime
+          </a>
+          <a href="">
+            <span>
+              <FaHeartPulse />
+            </span>
+            Bien être
+          </a>
+        </div>
+        <div id="nav-login">
+          <a href="">
+            <span>
+              <IoMdSettings />
+            </span>
+            Paramètres
+          </a>
+          <a href="">
+            <span>
+              <SlLogin />
+            </span>
+            Se connecter
+          </a>
+        </div>
+      </nav>
+    </aside>
   );
 }
