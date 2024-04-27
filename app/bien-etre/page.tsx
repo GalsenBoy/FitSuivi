3b5b273ccc5f314b01eed46ce9558a742eee4e25
bilaly <billycsk@gmail.com>
2024-04-27 13:06:38 +0200

    ajout des liens sur les différentes pages de bien être

--- app/bien-etre/page.tsx
+++ app/bien-etre/page.tsx
@@ -1,44 +1,8 @@
+import Link from "next/link";
+import { regimes } from "./bien-etre";
 import "./bien-etre.scss";
 import PageCard from "@/components/page-card/PageCard";
 export default function BienEtre() {
-  const regimes = [
-    {
-      title: "Surveiller son alimentation",
-      image: "/healthy.webp",
-    },
-    {
-      title: "Pratiquer une activité physique régulière",
-      image: "/activite.webp",
-    },
-    {
-      title: "Boire de l'eau régulièrement",
-      image: "/eau.webp",
-    },
-    {
-      title: "Mieux dormir",
-      image: "/dormir.webp",
-    },
-    {
-      title: "Prendre du temps de se détendre",
-      image: "/detendre.webp",
-    },
-    {
-      title: "Avoir une attitude positive",
-      image: "/positif.webp",
-    },
-    {
-      title: "Eviter le tabac et l'alcool",
-      image: "/tabac.webp",
-    },
-    {
-      title: "Limiter le temps d'écran",
-      image: "/temps.webp",
-    },
-    {
-      title: "Prendre soin de sa santé mentale",
-      image: "/sante.webp",
-    },
-  ];
   return (
     <section>
       <h1 className="text-center text-3xl">Conseil de bien-être</h1>
@@ -50,7 +14,9 @@ export default function BienEtre() {
             alt={regime.title}
             padding="p-4"
           >
-            <p>{regime.title}</p>
+            <Link href={regime.url}>
+              <p>{regime.title}</p>
+            </Link>
           </PageCard>
         ))}
       </div>
